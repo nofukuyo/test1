da3babacd5461b41775c2bf77828700d18e87306
Fukuyo <nobutakf@amazon.co.jp>
2020-07-28 00:50:53 +0900

    Initial deployment of our empty pipeline

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -1,7 +1,20 @@
 #!/usr/bin/env node
+/*
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { CdkpipelinesDemoStack } from '../lib/cdkpipelines-demo-stack';
 
 const app = new cdk.App();
 new CdkpipelinesDemoStack(app, 'CdkpipelinesDemoStack');
+*/
+
+import { App } from '@aws-cdk/core';
+import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
+
+const app = new App();
+
+new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
+  env: { account: '827697008625', region: 'ap-northeast-1' },
+});
+
+app.synth();
